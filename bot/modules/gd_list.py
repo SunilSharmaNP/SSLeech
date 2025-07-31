e14014d8dba8122a21ae8f54f57221df10e352b4
Sunil Sharma <Arnil1718@gmail.com>
2025-07-31 08:37:43 +0530

    Update gd_list.py

--- bot/modules/gd_list.py
+++ bot/modules/gd_list.py
@@ -24,14 +24,14 @@ from bot.helper.themes import BotTheme
 
 async def list_buttons(user_id, isRecursive=True):
     buttons = ButtonMaker()
-    buttons.ibutton("Only Folders", f"list_types {user_id} folders {isRecursive}")
-    buttons.ibutton("Only Files", f"list_types {user_id} files {isRecursive}")
-    buttons.ibutton("Both", f"list_types {user_id} both {isRecursive}")
+    buttons.ibutton("𝐎ɴʟʏ 𝐅ᴏʟᴅᴇʀs", f"list_types {user_id} folders {isRecursive}")
+    buttons.ibutton("𝐎ɴʟʏ 𝐅ɪʟᴇs", f"list_types {user_id} files {isRecursive}")
+    buttons.ibutton("𝐁ᴏᴛʜ", f"list_types {user_id} both {isRecursive}")
     buttons.ibutton(
-        f"{'✅️' if isRecursive else ''} Recursive",
+        f"{'✅️' if isRecursive else ''} 𝐑ᴇᴄᴜʀsɪᴠᴇ",
         f"list_types {user_id} rec {isRecursive}",
     )
-    buttons.ibutton("Cancel", f"list_types {user_id} cancel")
+    buttons.ibutton("𝐂ᴀɴᴄᴇʟ", f"list_types {user_id} cancel")
     return buttons.build_menu(2)
 
 
@@ -64,34 +64,32 @@ async def select_type(_, query):
     key = message.reply_to_message.text.split(maxsplit=1)[1].strip()
     data = query.data.split()
     if user_id != int(data[1]):
-        return await query.answer(text="Not Yours!", show_alert=True)
+        return await query.answer(text="𝐍ᴏᴛ 𝐘ᴏᴜʀs!", show_alert=True)
     elif data[2] == "rec":
         await query.answer()
         isRecursive = not bool(eval(data[3]))
         buttons = await list_buttons(user_id, isRecursive)
-        return await editMessage(message, "<b>Choose drive list options:</b>", buttons)
+        return await editMessage(message, "<b>𝐂ʜᴏᴏsᴇ ᴅʀɪᴠᴇ ʟɪsᴛ ᴏᴘᴛɪᴏɴs:</b>", buttons)
     elif data[2] == "cancel":
         await query.answer()
-        return await editMessage(message, "<b>List has been canceled!</b>")
+        return await editMessage(message, "<b>𝐋ɪsᴛ ʜᴀs ʙᴇᴇɴ ᴄᴀɴᴄᴇʟᴇᴅ!</b>")
     await query.answer()
     item_type = data[2]
     isRecursive = eval(data[3])
     await editMessage(message, BotTheme("LIST_SEARCHING", NAME=key))
     await _list_drive(key, message, user_id, item_type, isRecursive)
 
-
 async def drive_list(_, message):
     args = message.text.split() if message.text else ["/cmd"]
     if len(args) == 1:
-        return await sendMessage(message, "<i>Send a search key along with command</i>")
+        return await sendMessage(message, "<i>𝐒ᴇɴᴅ ᴀ sᴇᴀʀᴄʜ ᴋᴇʏ ᴀʟᴏɴɢ ᴡɪᴛʜ ᴄᴏᴍᴍᴀɴᴅ</i>")
     user_id = message.from_user.id
     msg, btn = await checking_access(user_id)
     if msg is not None:
         await sendMessage(message, msg, btn.build_menu(1))
         return
     buttons = await list_buttons(user_id)
-    await sendMessage(message, "<b>Choose drive list options:</b>", buttons, "IMAGES")
-
+    await sendMessage(message, "<b>𝐂ʜᴏᴏsᴇ ᴅʀɪᴠᴇ ʟɪsᴛ ᴏᴘᴛɪᴏɴs:</b>", buttons, "IMAGES")
 
 bot.add_handler(
     MessageHandler(
