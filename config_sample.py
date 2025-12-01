298eec01a3a2ad6deb541ad0739fec9cb764a42e
Sunil Sharma <Arnil1718@gmail.com>
2025-12-01 20:56:28 +0545

    Update config_sample.py

--- config_sample.py
+++ config_sample.py
@@ -11,7 +11,7 @@ TG_PROXY = (
     {}
 )  # {"scheme": ”socks5”, "hostname": ””, "port": 1234, "username": ”user”, "password": ”pass”}
 USER_SESSION_STRING = ""
-CMD_SUFFIX = "x"
+CMD_SUFFIX="x"
 AUTHORIZED_CHATS = ""
 SUDO_USERS = "2032446867"
 STATUS_LIMIT = 10
