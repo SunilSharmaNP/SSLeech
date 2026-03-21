fa84a7dff2fe271d39cc5fab126b3b4feaea608c
Sunil Sharma <Arnil1718@gmail.com>
2026-03-21 09:30:20 +0530

    Update bot_commands.py

--- bot/helper/telegram_helper/bot_commands.py
+++ bot/helper/telegram_helper/bot_commands.py
@@ -88,6 +88,8 @@ class _BotCommands:
         self.MyDramaListCommand = f"mdl{CMD_SUFFIX}"
         self.GDCleanCommand = [f"gdclean{CMD_SUFFIX}", f"gc{CMD_SUFFIX}"]
         self.BroadcastCommand = [f"broadcast{CMD_SUFFIX}", f"bc{CMD_SUFFIX}"]
+        self.MVidCommand = f"vtm{CMD_SUFFIX}"
+        self.LVidCommand = f"vtl{CMD_SUFFIX}"
 
 
 BotCommands = _BotCommands()
