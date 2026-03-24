e681de91286ccbc8700b0c618a3cd4d73c6c11d8
Sunil Sharma <Arnil1718@gmail.com>
2026-03-24 19:22:34 +0530

    Update video_tools.py

--- bot/modules/video_tools.py
+++ bot/modules/video_tools.py
@@ -1,134 +1,68 @@
-"""
-Video Tools - Anime-Leech style implementation
-Handles /vtl and /vtm commands for video processing
-"""
-
-from pyrogram import Client
-from pyrogram.filters import command
-from pyrogram.handlers import MessageHandler
-from pyrogram.types import Message
+from aiofiles.os import path as aiopath
+from asyncio import sleep
 from secrets import token_urlsafe
-from time import sleep
 
-from bot import bot, config_dict, LOGGER
-from bot.helper.ext_utils.bot_utils import new_task, is_url
+from pyrogram.handlers import MessageHandler
+from pyrogram.filters import command
+
+from bot import bot, LOGGER
+from bot.helper.ext_utils.bot_utils import new_task, arg_parser
+from bot.helper.ext_utils.links_utils import is_url, get_url_name, get_link
 from bot.helper.listeners.tasks_listener import TaskListener
 from bot.helper.telegram_helper.bot_commands import BotCommands
 from bot.helper.telegram_helper.filters import CustomFilters
-from bot.helper.telegram_helper.message_utils import (
-    sendMessage, editMessage, deleteMessage, auto_delete_message
-)
+from bot.helper.telegram_helper.message_utils import sendMessage, editMessage, deleteMessage, auto_delete_message
+from bot.helper.video_utils.executor import VidEcxecutor
 from bot.helper.video_utils.selector import SelectMode
-from bot.helper.video_utils.executor import VidExecutor, get_metavideo
 
 
 class VidTools(TaskListener):
-    """Video Tools handler - processes videos with user-selected operations"""
-    
-    def __init__(self, client: Client, message: Message, isLeech=False):
+    def __init__(self, client, message, isLeech=False, **kwargs):
         self.message = message
         self.client = client
         self.isLeech = isLeech
         super().__init__()
-    
+
     @new_task
     async def newEvent(self):
-        """Main entry point for video tools operation"""
         text = self.message.text.split('\n')
-        await self.getTag(text)
-        
-        # Extract link from message
-        reply_to = self.message.reply_to_message
-        
-        # Try to get link from command arguments
-        link_parts = text[0].split(' ', 1)
-        self.link = link_parts[1] if len(link_parts) > 1 else None
-        
-        # If no link in command, try reply message
-        if not self.link and reply_to and reply_to.text:
-            self.link = reply_to.text.split('\n', 1)[0].strip()
-        
-        # Validate link
-        if not self.link or not is_url(self.link):
-            msg = await sendMessage('❌ Send link or use command with link!', self.message)
-            await auto_delete_message(self.message, msg)
+        input_list = text[0].split(' ')
+        arg_base = {'link': ''}
+        args = arg_parser(input_list[1:], arg_base)
+        self.link = args['link'] or get_link(self.message)
+        if not is_url(self.link):
+            await sendMessage('Send command along with link or by reply to the link!', self.message)
             return
-        
-        # Get video metadata
-        metadata = await get_metavideo(self.link)
-        if not metadata or not metadata[0]:
-            msg = await sendMessage('❌ Failed getting video metadata!', self.message)
-            await auto_delete_message(self.message, msg)
+        self.vidMode = await SelectMode(self, True).get_buttons()
+        if not self.vidMode:
             return
-        
-        # Show video tools menu
-        self.editable = await sendMessage('🎬 <b>Loading Video Tools Menu...</b>', self.message)
-        
-        selector = SelectMode(self, is_link=True)
-        vidMode = await selector.get_buttons()
-        
-        if not vidMode:
-            await deleteMessage(self.editable)
-            msg = await sendMessage('❌ Video tools selection cancelled', self.message)
-            await auto_delete_message(self.message, msg)
-            return
-        
-        # Update name if renamed in selector
-        if vidMode[1]:
-            self.name = vidMode[1]
-        elif not self.name:
-            # Extract filename from URL or use default
-            try:
-                self.name = self.link.split('/')[-1].split('?')[0] or 'video'
-            except:
-                self.name = 'video'
-        
-        await editMessage('⏳ <b>Processing video...</b>', self.editable)
-        
+        self.name = get_url_name(self.link)
+        self.editable = await sendMessage('<i>Checking request, please wait...</i>', self.message)
+        await sleep(1)
         try:
-            # Execute video processing
-            gid = token_urlsafe(12)
-            executor = VidExecutor(self, self.link, gid, metadata)
-            executor.mode = vidMode[0]
-            executor.extra_data = vidMode[2]
-            
-            out_path = await executor.execute()
-            
-            if not out_path:
-                await editMessage('❌ Video processing failed!', self.editable)
-                return
-            
-            await deleteMessage(self.editable)
-            await self.onDownloadComplete()
-            
+            await self.beforeStart()
         except Exception as e:
-            LOGGER.error(f"Video tools error: {str(e)}", exc_info=True)
-            await editMessage(f'❌ Error: {str(e)}', self.editable)
+            await editMessage(str(e), self.editable)
+            return
+        await deleteMessage(self.editable)
+        gid = token_urlsafe(12)
+        out_pah = await VidEcxecutor(self, self.link, gid, False).execute()
+        if not out_pah:
+            return
+        if not await aiopath.exists(str(out_pah)):
+            self.name = self.vidMode[1] or self.name
+            await self.onUploadError('No file(s) to upload')
+            return
+        await self.onDownloadComplete()
 
 
-@new_task
-async def mirror_vidtools(_, message: Message):
-    """Handle /vtm command (mirror with video tools)"""
-    await VidTools(_, message, isLeech=False).newEvent()
+async def mirror_vidtools(client, message):
+    VidTools(client, message).newEvent()
 
 
-@new_task
-async def leech_vidtools(_, message: Message):
-    """Handle /vtl command (leech with video tools)"""
-    await VidTools(_, message, isLeech=True).newEvent()
+async def leech_vidtools(client, message):
+    VidTools(client, message, isLeech=True).newEvent()
 
 
-# Register handlers
-bot.add_handler(
-    MessageHandler(
-        mirror_vidtools,
-        filters=command(BotCommands.MVidCommand) & CustomFilters.authorized
-    )
-)
-
-bot.add_handler(
-    MessageHandler(
-        leech_vidtools,
-        filters=command(BotCommands.LVidCommand) & CustomFilters.authorized
-    )
-)
+bot.add_handler(MessageHandler(mirror_vidtools, filters=command(BotCommands.MVidCommand) & CustomFilters.authorized))
+bot.add_handler(MessageHandler(leech_vidtools, filters=command(BotCommands.LVidCommand) & CustomFilters.authorized))
