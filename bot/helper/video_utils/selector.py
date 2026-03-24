3cb678d3ce7f958f3b734a324f4535623bd81c87
Sunil Sharma <Arnil1718@gmail.com>
2026-03-24 19:49:45 +0530

    Update selector.py

--- bot/helper/video_utils/selector.py
+++ bot/helper/video_utils/selector.py
@@ -11,7 +11,7 @@ from pyrogram.types import Message, CallbackQuery
 
 from bot import config_dict, VID_MODE
 from bot.helper.ext_utils.bot_utils import new_task, new_thread, sync_to_async
-from bot.helper.ext_utils.files_utils import clean_target
+from bot.helper.ext_utils.fs_utils import clean_target
 from bot.helper.ext_utils.links_utils import is_media
 from bot.helper.ext_utils.status_utils import get_readable_time
 from bot.helper.listeners import tasks_listener as task
