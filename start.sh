c0958b1b5b94a277c8bef4c303c28e9d2e80d0fd
Sunil Sharma <Arnil1718@gmail.com>
2026-06-04 20:15:45 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+set -e
 source /usr/src/app/.venv/bin/activate
 cd /usr/src/app
 export PYTHONPATH="/usr/src/app:${PYTHONPATH}"
-python3 update.py && python3 -m bot
+python3 update.py
+exec python3 -m bot
