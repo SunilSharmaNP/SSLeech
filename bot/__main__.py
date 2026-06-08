7b6e03aed2e9e7c1f94aa77118621dda81d85562
Sunil Sharma <Arnil1718@gmail.com>
2026-06-08 05:48:05 +0545

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -404,8 +404,7 @@ async def main():
         set_commands(bot),
         log_check(),
     )
-    if not config_dict.get("DISABLE_TORRENTS"):
-        await sync_to_async(start_aria2_listener, wait=False)
+    await sync_to_async(start_aria2_listener, wait=False)
     bot.loop.create_task(search_images())
 
     bot.add_handler(
