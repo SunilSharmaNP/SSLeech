39b9b371cee0b7224b45d417a5d212d96a7940c3
Sunil Sharma <Arnil1718@gmail.com>
2026-07-08 20:19:50 +0545

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -650,13 +650,13 @@ async def compare_versions(v1, v2):
 async def get_stats(event, key="home"):
     user_id = event.from_user.id
     btns = ButtonMaker()
-    btns.ibutton("🔙 Back", f"wzmlx {user_id} stats home")
+    btns.ibutton("🔙 Back", f"wzmlx {user_id} stats home", icon_custom_emoji_id=5416117059207572332)
     if key == "home":
         btns = ButtonMaker()
-        btns.ibutton("🤖 Bot Stats", f"wzmlx {user_id} stats stbot")
-        btns.ibutton("🖥️ OS Stats", f"wzmlx {user_id} stats stsys")
-        btns.ibutton("📊 Repo Stats", f"wzmlx {user_id} stats strepo")
-        btns.ibutton("⚡ Bot Limits", f"wzmlx {user_id} stats botlimits")
+        btns.ibutton("🤖 Bot Stats", f"wzmlx {user_id} stats stbot", icon_custom_emoji_id=5431815506048155538)
+        btns.ibutton("🖥️ OS Stats", f"wzmlx {user_id} stats stsys", icon_custom_emoji_id=5282843764451195532)
+        btns.ibutton("📊 Repo Stats", f"wzmlx {user_id} stats strepo", icon_custom_emoji_id=5190806721286657692)
+        btns.ibutton("⚡ Bot Limits", f"wzmlx {user_id} stats botlimits", icon_custom_emoji_id=5456140674028019486)
         msg = "⌬ <b><i>Bot & OS Statistics!</i></b>"
     elif key == "stbot":
         total, used, free, disk = disk_usage("/")
@@ -774,7 +774,7 @@ async def get_stats(event, key="home"):
             UT=("∞" if (val := config_dict["USER_MAX_TASKS"]) == "" else val),
             BT=("∞" if (val := config_dict["BOT_MAX_TASKS"]) == "" else val),
         )
-    btns.ibutton("❌ Close", f"wzmlx {user_id} close")
+    btns.ibutton("❌ Close", f"wzmlx {user_id} close", icon_custom_emoji_id=5447644880824181073)
     return msg, btns.build_menu(2)
 
 
